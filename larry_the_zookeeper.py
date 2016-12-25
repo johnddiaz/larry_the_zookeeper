5d90dc32c67147fb9ee736bae64bba9e8be1a72d
alanxoc3 <alanxoc3@gmail.com>
2016-12-24 17:46:18 -0800

    Fixed no colon bug.
    If you enter a file without a colon on a line, then there was an error.

--- larry_the_zookeeper.py
+++ larry_the_zookeeper.py
@@ -10,13 +10,16 @@ def format_quote(quote): #trying to figure out how i would use this
     return quote[0] + ': ' + quote[1]
         
 def store_file(filename):
-    file = open(filename)#directory
+    file = open(filename) #directory
     file_content = file.readlines()
     dialog = []
     
     for line in file_content:
-        ind = line.index(':')
-        dialog.append( (line[0:ind].strip(), line[ind+1:].strip()) )
+        try:
+            ind = line.index(':')
+            dialog.append( (line[0:ind].strip(), line[ind+1:].strip()) )
+        except:
+            dialog.append( line.strip() )
     return dialog
 
 def print_delayed_text(text, delay):
@@ -26,8 +29,12 @@ def print_delayed_text(text, delay):
         time.sleep(delay)
 
 def print_quote(quote):
-    print_delayed_text(quote[0] + ': ', .05)
-    print_delayed_text(quote[1] + '\n', .2)
+    if type(quote) == tuple:
+        print_delayed_text(quote[0] + ': ', .05)
+        print_delayed_text(quote[1] + '\n', .2)
+    else:
+        print_delayed_text(quote, .05)
+        print_delayed_text('\n', .2)
 
 def main():
     if len(sys.argv) != 2:
